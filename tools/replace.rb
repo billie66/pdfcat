de477310f657c21e2ffca0c2d5bc5370d202f305
Billie Zhang <billiecoder@gmail.com>
2013-05-19 10:03:12 +0800

    vim :set expandtab

--- tools/replace.rb
+++ tools/replace.rb
@@ -9,54 +9,54 @@ root = File.join(here, '..')
 outdir = File.join(root, 'pandoc')
 
 def replace(string, &block)
-	string.instance_eval do
-		alias :s :gsub!
-		instance_eval(&block)
-	end
-	string
+  string.instance_eval do
+    alias :s :gsub!
+    instance_eval(&block)
+  end
+  string
 end
 
 def pre_markdown(string)
-	replace(string) do
-		# Use '#name#' instead of 'title: name' 
+  replace(string) do
+    # Use '#name#' instead of 'title: name' 
     s /-{3}\nlayout: book-zh\ntitle: (.*?) ?\n-{3}/, '#\1#'
 
-		# Use markdown syntax instead of html ordered list
-		s /<ol>(.*?)<\/ol>/m do
-			t = $1.gsub(/<li><?p?>?(.*?)<?\/?p?>?<\/li>/m, '1. \1')
-	  end
+    # Use markdown syntax instead of html ordered list
+    s /<ol>(.*?)<\/ol>/m do
+      t = $1.gsub(/<li><?p?>?(.*?)<?\/?p?>?<\/li>/m, '1. \1')
+    end
 
-		# Use '*' instead of html unordered list
-		s /<ul>(.*?)<\/ul>/m do
-			t = $1.gsub(/<li><?p?>?(.*?)<?\/?p?>?<\/li>/m, '* \1')
-	  end
-		
-		# Delete <div> block
-		s /<div class="single">(.*?)<\/div>/m do
-		 t =	"---\n" << $1 << "---\n" 
-		end
+    # Use '*' instead of html unordered list
+    s /<ul>(.*?)<\/ul>/m do
+      t = $1.gsub(/<li><?p?>?(.*?)<?\/?p?>?<\/li>/m, '* \1')
+    end
+    
+    # Delete <div> block
+    s /<div class="single">(.*?)<\/div>/m do
+      t = "---\n" << $1 << "---\n" 
+    end
 
-	  s(/<\/?h3>/, '**').gsub!(/<\/?p>/, "\n")
+    s(/<\/?h3>/, '**').gsub!(/<\/?p>/, "\n")
 
-		# Remove html table
-		s /<table ?.*?>(.*?)<\/table>/m do
-			t = $1.gsub(/<\/t[dh]>\n<t[dh] ?.*?>/, ' <==> ')
-			t = t.gsub(/<caption ?.*?>(.*?)<\/caption>/, 'caption: \1').
-					gsub(/<th ?.*?>(.*?)<\/th>/, 'th: \1').
-					gsub(/<td ?.*?>(.*?)<\/td>/m, 'td: \1').
-					gsub(/<.*?>\n?/, '')
-			end
-	end	
+    # Remove html table
+    s /<table ?.*?>(.*?)<\/table>/m do
+      t = $1.gsub(/<\/t[dh]>\n<t[dh] ?.*?>/, ' <==> ')
+      t = t.gsub(/<caption ?.*?>(.*?)<\/caption>/, 'caption: \1').
+          gsub(/<th ?.*?>(.*?)<\/th>/, 'th: \1').
+          gsub(/<td ?.*?>(.*?)<\/td>/m, 'td: \1').
+          gsub(/<.*?>\n?/, '')
+    end
+  end 
 end
 
 mkdir_p(outdir) if !Dir.exist?(outdir)
 
 md = Dir["#{root}/zh/*.md"].sort.map do |file|
-	File.read(file)
+  File.read(file)
 end.join("\n\n")
 
 pre = pre_markdown(md)
 
 File.open("#{here}/tmp", 'w') do |f|
-	f.write(pre)
+  f.write(pre)
 end
